1a7cd3e1159b3b960797868ebddf58b3f52acb3b
RamiB1234 <rbisthebest@hotmail.com>
2021-09-27 04:00:03 +0300

    Implement basic gameplay

---
+++ Assets/Scripts/Lander.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lander : MonoBehaviour
+{
+    public float speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float verticalMovement = Input.GetAxis("Vertical") * speed;
+        float horizontalMovement = Input.GetAxis("Horizontal") * speed;
+        if(horizontalMovement>0)
+        {
+            GetComponent<Animator>().SetBool("MovingRight", true);
+            GetComponent<Animator>().SetBool("MovingLeft", false);
+        }
+        else if (horizontalMovement < 0)
+        {
+            GetComponent<Animator>().SetBool("MovingLeft", true);
+            GetComponent<Animator>().SetBool("MovingRight", false);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("MovingRight", false);
+            GetComponent<Animator>().SetBool("MovingLeft", false);
+        }
+
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(horizontalMovement, verticalMovement));
+    }
+}
---
+++ Assets/Scripts/Meteoroid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Meteoroid : MonoBehaviour
+{
+    public float velocity = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(0.0f, 0.0f, Random.Range(0.0f, 360.0f));
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, velocity);
+        GetComponent<Rigidbody2D>().angularVelocity = Random.value * 20;
+
+        // Tilt
+        Vector3 euler = transform.localEulerAngles;
+        euler.z = Mathf.Lerp(euler.z, 0, 2.0f * Time.deltaTime);
+        transform.localEulerAngles = euler;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
