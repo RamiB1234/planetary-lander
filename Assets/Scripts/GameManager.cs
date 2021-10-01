fbc5c89621dddfc4320eab23564ce35cdd799881
Rami Bokhari <rbisthebest@hotmail.com>
2021-10-01 18:40:41 +0300

    Show how to play menu the first time game is started

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,10 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager GM;
+    public static bool gameStarted = false;
     public bool missionFailed = false;
     public GameObject missionFailedMenu;
+    public GameObject howToPlayMenu;
 
     void Awake()
     {
@@ -18,12 +20,22 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(gameStarted==false)
+        {
+            Time.timeScale = 0;
+            howToPlayMenu.SetActive(true);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(gameStarted)
+        {
+            Time.timeScale = 1;
+            howToPlayMenu.SetActive(false);
+
+        }
         if(missionFailed)
         {
             missionFailedMenu.SetActive(true);
@@ -37,5 +49,10 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 1;
         var scene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(scene.name);
+    }
+
+    public void StartGame()
+    {
+        gameStarted = true;
     }
 }
